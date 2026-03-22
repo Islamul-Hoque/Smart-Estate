cbe73f91597f430d61571f6a1bcea99b7123d399
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-03-22 14:05:55 +0600

    404 error page added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const notFound = () => {
+    return (
+        <div className='bg-amber-100 text-3xl text-gray-800'>
+            404 error
+        </div>
+    );
+};
+
+export default notound;
